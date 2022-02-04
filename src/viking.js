51da1fc5a37f1412b56a6c598694477f1d9948bf
francisco cano <frankiecano24@gmail.com>
2022-02-03 21:26:27 -0500

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,19 +1,106 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor (health, strength) {
+    this.health = health;
+    this.strength = strenth;
+    this.attack = attack;
+    this.recieveDamage = recieveDamage;
+    this.maxDamage = this.maxDamage
+    attack() 
+      if (this.attack > 0 && this.attack - 50 > -10) {
+          this.attack -= 50;
+      } else if (this.attack - 5 < 0) {
+          this.attack = 0;
+      }
+  }
+  recieveDamage() {
+    if (this.attack < this.maxDamage && (this.attack + this.recieveDamage) < this.maxDamage) {
+     this.attack += this.recieveDamage;
+     } else if ( this.attack+ this.recieveDamage > this.maxDamage 
+     this.attack = this.maxDamage
+    }
+    this.speed += this.accelerationAmount;
+}
+}
+  }
+}
 
 // Viking
-class Viking {}
+class Viking {constructor (health, strength) {
+  this.health = health;
+  this.strength = strenth;
+  this.attack = attack;
+  this.recieveDamage = recieveDamage;
+  this.maxDamage = this.maxDamage
+  attack() {
+    if (this.attack > 0 && this.attack - 53 > -12) {
+        this.attack -= 53;
+    } else if (this.attack - 5 < 0) {
+        this.attack = 0;
+    }
+}
+recieveDamage() {
+  if (this.attack < this.maxDamage && (this.attack + this.recieveDamage) < this.maxDamage) {
+   this.attack += this.recieveDamage;
+   } else if ( this.attack+ this.recieveDamage > this.maxDamage {
+   this.attack = this.maxDamage
+  }
+  this.speed += this.accelerationAmount;
+}
+}
+}
+}
+}
 
 // Saxon
-class Saxon {}
+class Saxon {constructor (health, strength) {
+  this.health = health;
+  this.strength = strenth;
+  this.attack = attack;
+  this.recieveDamage = recieveDamage;
+  this.maxDamage = this.maxDamage
+  attack() {
+    if (this.attack > 0 && this.attack - 48> -15) {
+        this.attack -= 48;
+    } else if (this.attack - 5 < 0) {
+        this.attack = 0;
+    }
+}
+recieveDamage() {
+  if (this.attack < this.maxDamage && (this.attack + this.recieveDamage) < this.maxDamage) {
+   this.attack += this.recieveDamage;
+   } else if ( this.attack+ this.recieveDamage > this.maxDamage {
+   this.attack = this.maxDamage
+  }
+  this.speed += this.accelerationAmount;
+}
+}
+}
+}
+}
 
 // War
-class War {}
+class War {let arrayOfWar = [];
+  const warArray = ["Soldier", "Viking", "Saxon",];
+  
+  for (let i = 0; i < 7; i++) {
+      arrayOfWar.push(
+          new War(
+              4,
+              warArray[Math.floor(Math.random() * warArray.length)],
+              180,
+              8,
+              3,
+              "Weapon",
+              "Warrior"
+          )
+      );
+  }
+  console.log({ oneWar: arrayOfWar[3] });}
 
 
 
-// The following is required to make unit tests work.
-/* Environment setup. Do not modify the below code. */
+/
 if (typeof module !== 'undefined') {
   module.exports = { Soldier, Viking, Saxon, War };
 }
